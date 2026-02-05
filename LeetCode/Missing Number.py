92f7a456678317876ee024eb70a8235cc0b823ee
Sapientia01 <hikochala@gmail.com>
2026-02-05 11:22:21 +0300

    Updating Missing number

--- LeetCode/Missing Number.py
+++ LeetCode/Missing Number.py
@@ -4,8 +4,18 @@ class Solution:
         n = len(nums)
         total_sum =  n * (n+1) // 2
         return total_sum - sum(nums)
+    
 
-# O(n) space and O(n) Time
+#O(n) space and O(n) Time  using set 
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int: 
+        actual_nums = set(list(range(len(nums)+1)))
+        missing_number = actual_nums - set(nums)
+        return missing_number.pop()
+
+
+
+# O(n) space and O(n) Time using list
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:        
         nums_range = [0 for i in range(len(nums)+1)] 
