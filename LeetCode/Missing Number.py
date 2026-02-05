3a8a89d7c163cdc0717782b6e5abe46424059602
Sapientia01 <hikochala@gmail.com>
2026-02-05 09:37:25 +0300

    a

--- LeetCode/Missing Number.py
+++ LeetCode/Missing Number.py
@@ -1,3 +1,15 @@
+# O(1) space and O(n) Time
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:        
+        n = len(nums)
+        total_sum =  n * (n+1) // 2
+
+        for num in nums:
+            total_sum -= num
+
+        return total_sum
+
+# O(n) space and O(n) Time
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:        
         nums_range = [0 for i in range(len(nums)+1)] 
