240d2f8593a6436454dd299fde08507190693bf1
Sapientia01 <hikochala@gmail.com>
2026-02-18 19:20:09 +0300

    adding problem

--- LeetCode/Sort the People.py
+++ LeetCode/Sort the People.py
@@ -1,5 +1,35 @@
 # Question link : https://leetcode.com/problems/sort-the-people/description/
 
+# Question link : https://leetcode.com/problems/sort-the-people/description/
+
+#  Using Count sorting
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        n = len(names)
+        max_len = max(*heights) +1
+
+        if n > 1:
+            counting = [0]* max_len
+
+            for i,height in enumerate(heights):
+                counting[height] = names[i]
+
+        return [name for name in counting if name != 0][::-1]
+
+
+#  Using insertion sorting
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        n = len(names)
+
+        for i in range(1,n):
+            for j in range(i, 0,-1):
+                if heights[j] >  heights[j-1]:
+                     heights[j] ,  heights[j-1] =  heights[j-1] ,  heights[j] 
+                     names[j],  names[j-1] =  names[j-1] ,  names[j]
+
+        return names
+
 #  Using selection sorting
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
@@ -26,6 +56,8 @@ class Solution:
                      heights[j] ,  heights[j-1] =  heights[j-1] ,  heights[j] 
                      names[j],  names[j-1] =  names[j-1] ,  names[j]
                      swapped = True 
+                else:
+                    break
 
             if not swapped:
                 break
