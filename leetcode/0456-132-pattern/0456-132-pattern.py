a20ea35dbc1858182d28b84d20e5da85c7ba021b
Chala Hiko <hikoChala@gmail.com>
2026-03-13 16:39:09 +0300

    Add solution for 132 Pattern

---
+++ leetcode/0456-132-pattern/0456-132-pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        pattern = []
+        third = "null"
+
+        for num in nums[::-1]:
+            if third != "null" and third > num:
+                return True
+
+            while pattern and num > pattern[-1]:
+                third = pattern.pop()
+
+            pattern.append(num)
+
+        return False
