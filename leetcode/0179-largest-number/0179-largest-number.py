002835420ec80cb2eddf4c2302a0105e5d3a8dfb
Chala Hiko <hikoChala@gmail.com>
2026-03-13 16:11:40 +0300

    Add solution for Largest Number

---
+++ leetcode/0179-largest-number/0179-largest-number.py
@@ -0,0 +1,14 @@
+    
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        n = len(nums)
+        nums = list(map(str,nums))
+
+        for i in range(n):
+            for j in range(i+1,n):
+                a = nums[i]
+                b = nums[j]
+                if b+a > a+b:
+                    nums[i], nums[j] = nums[j], nums[i]
+
+        return str(int("".join(nums)))
